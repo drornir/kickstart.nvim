01a41a28705e1954a45521027ac5b6e64ed2d63a
dror nir <me@drornir.dev>
2024-02-10 12:56:47 +0200

    configure tab size

--- init.lua
+++ init.lua
@@ -44,6 +44,13 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- My Opts
+
+-- Tab Size
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
